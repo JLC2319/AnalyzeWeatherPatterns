4e358621da91bd2c01433a1b74d55146991e6823
JLC2319 <jamescraig2319@gmail.com>
2022-08-17 08:32:04 -0500

    historical weather data

--- fetch4weather.js
+++ fetch4weather.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY
 
 //locationObj: {city:String, lat:Number, lon:Number}
-async function getWeatherData(lat, lon, type='hour', start= new Date('2012/01/01'), end= new Date('2022/01/01'),API_KEY= OPEN_WEATHER_MAP_API_KEY){
-    const URL = `https://history.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${lon}&type=${type}&start=${start}&end=${end}&appid=${API_KEY}`
+async function getHistoricalWeatherData(lat, lon, start= new Date('2022/08/15').getUTCMilliseconds(),API_KEY= OPEN_WEATHER_MAP_API_KEY){
+    const URL = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${start}&appid=${API_KEY}`
 
     return fetch(URL)
         .then((response) => (response.json()))
@@ -36,4 +36,4 @@ async function getCurrentWeatherData(lat, lon,API_KEY= OPEN_WEATHER_MAP_API_KEY)
 }
 
 
-module.exports = {getWeatherData, getCurrentWeatherData}
+module.exports = {getWeatherData: getHistoricalWeatherData, getCurrentWeatherData}
--- fetch4weather.spec.test.js
+++ fetch4weather.spec.test.js
@@ -1,13 +1,13 @@
-const { getWeatherData, getCurrentWeatherData } = require('./fetch4weather')
+const { getWeatherData: getHistoricalWeatherData, getCurrentWeatherData } = require('./fetch4weather')
 require('dotenv').config();
 const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY
 console.log(OPEN_WEATHER_MAP_API_KEY)
 const city = "Dallas"
 const lat = "32.7", lon = "-96.7";
 (async function () {
-    let data = await getCurrentWeatherData(lat, lon, OPEN_WEATHER_MAP_API_KEY)
+    let data = await getHistoricalWeatherData(lat, lon, (new Date('01/01/2022') - new Date(0))/1000, OPEN_WEATHER_MAP_API_KEY) //getCurrentWeatherData(lat, lon)
 
-    console.log(data)
+    console.log(data.data, console.log(new Date(data.data[0].dt*1000)))
 
 
 })()
