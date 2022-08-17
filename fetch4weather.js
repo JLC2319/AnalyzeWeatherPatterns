c4c6b4b2c705a5ee17079c9ef2fcb3e3c46ec60f
JLC2319 <jamescraig2319@gmail.com>
2022-08-17 07:37:24 -0500

    new api endpoint

--- fetch4weather.js
+++ fetch4weather.js
@@ -2,12 +2,38 @@ require('dotenv').config();
 const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY
 
 //locationObj: {city:String, lat:Number, lon:Number}
-async function getWeatherData(city, lat, lon, type='hour', start= new Date('2012/01/01'), end= new Date('2022/01/01'),API_KEY= OPEN_WEATHER_MAP_API_KEY){
-    const URL = `http://history.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${lon}&type=${type}&start=${start}&end=${end}&appid=${API_KEY}`
+async function getWeatherData(lat, lon, type='hour', start= new Date('2012/01/01'), end= new Date('2022/01/01'),API_KEY= OPEN_WEATHER_MAP_API_KEY){
+    const URL = `https://history.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${lon}&type=${type}&start=${start}&end=${end}&appid=${API_KEY}`
 
     return fetch(URL)
         .then((response) => (response.json()))
 }
 
+async function getCurrentWeatherData(lat, lon,API_KEY= OPEN_WEATHER_MAP_API_KEY){
+    const URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&exclude=daily,minutely,hourly`
 
-module.exports = getWeatherData
+    return fetch(URL)
+        .then((response) => (response.json())).then((data)=>(data.current)).then((data)=>{
+            const transformedData = {
+                dt: new Date(data.dt*1000).toTimeString().slice(0,8),
+                sunrise: new Date(data.sunrise*1000).toTimeString().slice(0,8),
+                sunset: new Date(data.sunset*1000).toTimeString().slice(0,8),
+                temp: `${Math.round((data.temp-273.15)*(9/5)+32)} degrees`,
+                feelsLike: `${Math.round((data.feels_like-273.15)*(9/5)+32)} degrees`,
+                pressure: `${data.pressure} bar`,
+                humidity: `${data.humidity}%`,
+                dewPoint: `${Math.round((data.dew_point-273.15)*(9/5)+32)} degrees`,
+                visibility: `${data.visibility} meters`,
+                windSpeed: `${data.wind_speed} meters per second`,
+                windDeg: `${data.wind_deg} degrees (meteorological)`,
+                weatherDescription: `${data.weather.main} weather with a ${data.weather.description}`,
+        
+            }
+
+            return transformedData
+        
+        })
+}
+
+
+module.exports = {getWeatherData, getCurrentWeatherData}
--- fetch4weather.spec.test.js
+++ fetch4weather.spec.test.js
@@ -1,11 +1,13 @@
-const getWeatherData = require('./fetch4weather')
+const { getWeatherData, getCurrentWeatherData } = require('./fetch4weather')
 require('dotenv').config();
 const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY
-
+console.log(OPEN_WEATHER_MAP_API_KEY)
 const city = "Dallas"
 const lat = "32.7", lon = "-96.7";
-(async function() {
-let data = await getWeatherData(city, lat, lon)
+(async function () {
+    let data = await getCurrentWeatherData(lat, lon, OPEN_WEATHER_MAP_API_KEY)
 
-console.log(data)
+    console.log(data)
+
+
 })()
